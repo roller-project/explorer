6adce8cbb76200c64605b940e67d90bf83e28b52
Roller Davic <rollerdavic@gmail.com>
2018-09-20 14:48:35 +0700

    Test

--- routes/token.js
+++ routes/token.js
@@ -157,7 +157,7 @@ module.exports = function(req, res){
     if (after) {
       filter.after = after;
     }
-    web3.trace.filter(filter, function(err, tx) {
+    web3.eth.subscribe(filter, function(err, tx) {
       if(err || !tx) {
         console.error("TraceWeb3 error :" + err)
         res.write(JSON.stringify({"error": true}));
